9c2be64d0ea8d7865f4223ddb4da426c9b1c041e
miyehn <rainduym@gmail.com>
2024-09-17 20:56:19 -0700

    text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Falalalalala (wtf why)
+          Falalalalala
         </p>
       </header>
     </div>
